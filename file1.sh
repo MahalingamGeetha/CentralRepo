6701d62a54aeb2d3d30fe74e38839dc9e207a5b2
Maha <mahalingamgopinath1993@gmail.com>
2024-04-19 11:05:00 +0000

    Commit First Command File

---
+++ file1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#########################################################
+#Author Name - Mahalingam G
+#Date        - 19-04-2024
+#Version     - 1.0
+########################################################
+
+#To List the Process of the Machine
+set -x
+echo "To List the Process"
+ps -ef
+echo "First Command From Europe"
